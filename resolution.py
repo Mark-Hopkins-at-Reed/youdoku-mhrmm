10704e6c54969be6809e8096ec337cf20415072f
Mark Hopkins <hopkinsm@reed.edu>
2020-09-30 19:45:52 -0700

    Added solution.

--- resolution.py
+++ resolution.py
@@ -1,28 +1,140 @@
+import queue
+import cnf
+
 def resolve_symbol(clause1, clause2, symbol):
-    """Part 5, Question 2"""
-    pass
+    if symbol in clause1 and symbol in clause2:
+        if clause1[symbol] and not clause2[symbol]:
+            return clause1.remove(symbol) | clause2.remove(symbol)
+        if not clause1[symbol] and clause2[symbol]:
+            return clause1.remove(symbol) | clause2.remove(symbol)
+    return None
+
+def optimized_resolve_symbol(clause1, clause2, symbol):
+    """ Caller must ensure that the symbol appears in both clauses. """
+    if clause1[symbol] != clause2[symbol]:
+        return clause1.remove(symbol) | clause2.remove(symbol)
+    return None
 
 def resolve(clause1, clause2):
-    """Part 5, Question 3"""
-    pass
-    
-class ClauseQueue:
-    """Part 5, Question 4"""
-    pass
+    resolvents = []
+    for sym in clause1.syms & clause2.syms:
+       resolvent = optimized_resolve_symbol(clause1, clause2, sym)  
+       if resolvent is not None:
+           resolvents.append(resolvent)
+    return resolvents
 
-def resolution_closure(initial_unprocessed):
-    """Part 6, Questions 1 and 2"""
-    pass
+
+class ClauseQueue:
+    def __init__(self):
+        self.queue = queue.PriorityQueue()
+        self.priority_function = lambda clause: len(clause)
+        self.cached_clauses = set([])
+        
+    def push(self, clause):
+        if not clause in self.cached_clauses:
+            self.queue.put((self.priority_function(clause), clause))
+            self.cached_clauses.add(clause)
+            return True
+        else:
+            return False
+    
+    def pop(self):
+        if not self.empty():
+            return self.queue.get()[1]
+        else:
+            return None
+    
+    def empty(self):
+        return self.queue.empty()
+    
+    def num_generated(self):
+        return len(self.cached_clauses)
+
+
+def resolution_closure(initial_unprocessed, early_stopping=False,
+                       resolution_filter = lambda c1, c2: True):
+    processed = set([])    
+    unprocessed = ClauseQueue()
+    for c in initial_unprocessed:
+        if early_stopping and not bool(c):
+            return set([c])   
+        unprocessed.push(c)
+    while not unprocessed.empty():
+        next_to_process = unprocessed.pop()
+        for clause in processed:
+            if resolution_filter(clause, next_to_process):
+                for resolvent in resolve(next_to_process, clause):
+                    unprocessed.push(resolvent)                  
+                    if early_stopping and not bool(resolvent):
+                        return set([resolvent])   
+        processed.add(next_to_process)
+    return processed
+
 
 def full_resolution(sent):
-    """Part 6, Question 3"""
-    pass
+    return cnf.c('FALSE') not in resolution_closure(sent.clauses, 
+                                                    early_stopping = True)
+
+
 
 def unit_resolution_closure(initial_unprocessed, early_stopping=False):
-    """Part 9, Question 1"""
-    pass
+    resolution_filter = lambda c1, c2: len(c1) == 1 or len(c2) == 1
+    return resolution_closure(initial_unprocessed,
+                              early_stopping = early_stopping,
+                              resolution_filter = resolution_filter)
+
+
+class FifoClauseQueue:
+    def __init__(self):
+        self.queue = []
+        self.cached_clauses = set([])
+        
+    def push(self, clause):
+        if not clause in self.cached_clauses:
+            self.queue.append(clause)
+            self.cached_clauses.add(clause)
+            return True
+        else:
+            return False
+    
+    def pop(self):
+        if len(self.queue) > 0:
+            result, self.queue = self.queue[0], self.queue[1:]
+            return result
+        else:
+            return None
+    
+    def empty(self):
+        return len(self.queue) == 0
+    
+    def num_generated(self):
+        return len(self.cached_clauses)
 
     
 def limited_unit_resolution_closure(unit_clause, processed, early_stopping=False):
-    """Part 9, Question 2"""
-    pass
+    processed_unit_clauses = set([clause for clause in processed if len(clause) == 1])
+    processed = set(processed)
+    unprocessed = FifoClauseQueue()
+    unprocessed.push(unit_clause)
+    while not unprocessed.empty():
+        next_to_process = unprocessed.pop()
+        if len(next_to_process) == 1:
+            comparisons = processed
+        else:
+            comparisons = processed_unit_clauses
+        for clause in comparisons:
+            for resolvent in resolve(next_to_process, clause):
+                unprocessed.push(resolvent)
+                if early_stopping and not bool(resolvent):
+                    return set([resolvent])   
+        processed.add(next_to_process)
+        if len(next_to_process) == 1:
+            processed_unit_clauses.add(next_to_process)
+    return processed
+
+
+
+
+    
+
+
--- sudoku.py
+++ sudoku.py
@@ -1,36 +1,146 @@
-class SudokuBoard:
+import math
+import cnf
+import copy
+from search import dpll
+
+def irange(i, j):
+    return range(i, j+1)
+
+def lit(d, i, j, polarity = True):
     """
-    Part 4, Question 1
-    Part 4, Question 6
-    Part 7, Question 1
-    Part 7, Question 2
-    Part 10, Question 1
+    Creates a literal indicating whether cell (i, j) contains digit d.
     
     """
-    pass
+    literal = 'd{}_{}_{}'.format(d, i, j)
+    if not polarity:
+        literal = '!{}'.format(literal)
+    return literal
 
+class SudokuBoard:
+    
+    def __init__(self, matrix):
+        self.matrix = matrix
+        self.box_width = int(math.sqrt(len(self.matrix)))
+        err = "Improper dimensions for a Sudoku board!"
+        assert self.box_width == math.sqrt(len(self.matrix)), err
+        self.generic_clauses = list(set(self._generate_generic_clauses()))
+        
+    def _generate_generic_clauses(self):
+        num_symbols = self.box_width * self.box_width
+        clause_strs = []
+        for zone in self.zones():
+            for digit in irange(1, num_symbols):
+                clause_strs += exactly_one_clauses(zone, digit)
+        clause_strs += nonempty_clauses(self.box_width)
+        clauses = [cnf.c(clause) for clause in clause_strs]
+        return clauses
+        
+    def __str__(self):
+        row_strs = [''.join([str(digit) for digit in row]) for row in self.matrix]        
+        return '\n'.join(row_strs)
+ 
+    def rows(self):
+        def row_cells(i, row_length):
+            return set([(i, j) for j in irange(1, row_length)])
+        num_symbols = self.box_width * self.box_width
+        return [row_cells(row, num_symbols) for row in irange(1, num_symbols)]
+    
+    def columns(self):
+        def col_cells(j, col_length):
+            return set([(i, j) for i in irange(1, col_length)])
+        num_symbols = self.box_width * self.box_width
+        return [col_cells(col, num_symbols) for col in irange(1, num_symbols)]
+ 
+    def boxes(self):
+        def box_cells(a, b, box_width):
+            return set([(i+1,j+1) for i in range((a-1) * box_width, a * box_width)
+                                  for j in range((b-1) * box_width, b * box_width)])
+        return [box_cells(a, b, self.box_width) for a in irange(1, self.box_width)
+                                                 for b in irange(1, self.box_width)]
+    def zones(self):
+        return self.rows() + self.columns() + self.boxes()
+    
+    def contents(self):
+        clauses = []
+        for row in range(len(self.matrix)):
+            for col in range(len(self.matrix[row])):
+                digit = self.matrix[row][col]
+                if digit != 0:
+                    clause = cnf.c(lit(digit, row+1, col+1))
+                    clauses.append(clause)
+        return clauses
+    
+    def cnf(self):
+        clauses = self.generic_clauses + self.contents()
+        return cnf.Cnf(clauses)
+
+    def solvable(self, solver):
+        return solver(self.cnf())
+    
+    ## new
+    def solve(self):
+        def interpret_lit(l):
+            negate = (l[0] == "!")
+            if negate:
+                l = l[2:]
+            else:
+                l = l[1:]
+            d, i, j = l.split("_")
+            return d, i, j, negate
+        model = dpll(self.cnf())
+        if model is None:
+            return None
+        matrix = copy.deepcopy(self.matrix)
+        positive_literals = [l for l in model if model[l] == 1]
+        for l in positive_literals:
+            d, i, j, _ = interpret_lit(l)
+            matrix[int(i)-1][int(j)-1] = int(d)
+        return SudokuBoard(matrix)
+    
+    
 def at_least_clause(cells, d):
     """
-    Part 4, Question 3
     Encodes: "The following cells have at least 1 of digit d."
 
     """  
-    pass 
-
+    literals = [lit(d, i, j) for (i, j) in sorted(cells)]
+    return ' || '.join(literals)  
 
 def at_most_clauses(cells, d):
     """
-    Part 4, Question 4
     Encodes: "The following cells have at most 1 of digit d."
 
     """ 
-    pass
+    def all_pairs(seq):
+        for a in range(len(seq)):
+            for b in range(a+1, len(seq)):
+                yield seq[a], seq[b]
+    clauses = []         
+    for cell1, cell2 in all_pairs(sorted(cells)):
+        clauses.append('{} || {}'.format(lit(d, cell1[0], cell1[1], polarity=False), 
+                                         lit(d, cell2[0], cell2[1], polarity=False)))        
+    return clauses
 
-    
-def nonempty_clauses(cell_width):
+def exactly_one_clauses(cells, d):
     """
-    Part 4, Question 5
-    Encodes: "Every cell is non-zero."
+    Encodes: "The following cells have exactly 1 of digit d."
+
+    """
+    return [at_least_clause(cells, d)] + at_most_clauses(cells, d)     
+
+
+def nonempty_clauses(box_width):
+    def non_zero_cell(i, j):
+        """
+        Encodes: "Cell i, j is non-zero."
     
-    """ 
-    pass
+        """ 
+        literals = [lit(d, i, j) for d in irange(1, box_size)]
+        return ' || '.join(literals)         
+    box_size = box_width * box_width    
+    clauses = [non_zero_cell(row, col)
+               for row in irange(1, box_size)
+               for col in irange(1, box_size)]
+    return clauses
+                
+
